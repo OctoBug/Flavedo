3f81fbbe390b90f5e629ede99f2f6bebc8f915f5
Octobug <362499939@qq.com>
2016-08-20 17:39:07 +0800

    exchange privilege by call gate

--- pmtest.asm
+++ pmtest.asm
@@ -17,14 +17,15 @@ LABEL_DESC_CODE32:	Descriptor	0,		SegCode32Len - 1,	DA_C + DA_32	;非一致代�
 LABEL_DESC_CODE16:	Descriptor	0,		0FFFFh,			DA_C		;非一致代码段，16位
 LABEL_DESC_CODE_DEST:	Descriptor	0,		SegCodeDestLen - 1,	DA_C + DA_32	;非一致代码段，32位
 LABEL_DESC_CODE_RING3:	Descriptor	0,		SegCodeRing3Len - 1,	DA_C + DA_32 + DA_DPL3
-LABEL_DESC_DATA:	Descriptor	0,		DataLen-1,		DA_DRW		;数据段
+LABEL_DESC_DATA:	Descriptor	0,		DataLen - 1,		DA_DRW		;数据段
 LABEL_DESC_STACK:	Descriptor	0,		TopOfStack,		DA_DRWA + DA_32	;栈，32位
 LABEL_DESC_STACK3:	Descriptor	0,		TopOfStack3,		DA_DRWA + DA_32 + DA_DPL3
-LABEL_DESC_LDT:		Descriptor	0,		LDTLen-1,		DA_LDT		;LDT
+LABEL_DESC_LDT:		Descriptor	0,		LDTLen - 1,		DA_LDT		;LDT
+LABEL_DESC_TSS:		Descriptor	0,		TSSLen - 1,		DA_386TSS
 LABEL_DESC_VIDEO:	Descriptor	0B8000h,	0FFFFh,			DA_DRW + DA_DPL3;显存首地址
 
 ;门------------------------目标选择子		偏移	DCount	属性
-LABEL_CALL_GATE_TEST: Gate SelectorCodeDest,	0,	0,	DA_386CG + DA_DPL0
+LABEL_CALL_GATE_TEST: Gate SelectorCodeDest,	0,	0,	DA_386CG + DA_DPL3
 
 ;GDT结束
 
@@ -41,10 +42,11 @@ SelectorCodeRing3	equ	LABEL_DESC_CODE_RING3 - LABEL_GDT + SA_RPL3
 SelectorData		equ	LABEL_DESC_DATA - LABEL_GDT
 SelectorStack		equ	LABEL_DESC_STACK - LABEL_GDT
 SelectorStack3		equ	LABEL_DESC_STACK3 - LABEL_GDT + SA_RPL3
+SelectorTSS		equ	LABEL_DESC_TSS - LABEL_GDT
 SelectorLDT		equ	LABEL_DESC_LDT - LABEL_GDT
 SelectorVideo		equ	LABEL_DESC_VIDEO - LABEL_GDT
 
-SelectorCallGateTest	equ	LABEL_CALL_GATE_TEST - LABEL_GDT
+SelectorCallGateTest	equ	LABEL_CALL_GATE_TEST - LABEL_GDT + SA_RPL3
 ;END of [SECTION .gdt]
 
 [SECTION .data1]	;数据段
@@ -76,6 +78,41 @@ LABEL_STACK3:
 	times 512 db 0
 TopOfStack3	equ	$ - LABEL_STACK3 - 1
 ;END of [SECTION .s3]
+
+;TSS
+[SECTION .tss]
+ALIGN 32
+[BITS 32]
+LABEL_TSS:
+	DD	0			;Back
+	DD	TopOfStack		;0级堆栈
+	DD	SelectorStack		;
+	DD	0			;1级堆栈
+	DD	0			;
+	DD	0			;2级堆栈
+	DD	0			;
+	DD	0			;CR3
+	DD	0			;EIP
+	DD	0			;EFLAGS
+	DD	0			;EAX
+	DD	0			;ECS
+	DD	0			;EDX
+	DD	0			;EBX
+	DD	0			;ESP
+	DD	0			;EBP
+	DD	0			;ESI
+	DD	0			;EDI
+	DD	0			;ES
+	DD	0			;CS
+	DD	0			;SS
+	DD	0			;DS
+	DD	0			;FS
+	DD	0			;GS
+	DD	0			;LDT
+	DW	0		;调试陷阱标志
+	DW	$ - LABEL_TSS + 2	;I/O位图基址
+	DB	0FFh			;I/O位图结束标志
+TSSLen	equ	$ - LABEL_TSS
 
 [SECTION .s16]
 [BITS 16]		;指定目标处理器模式位16位
@@ -179,12 +216,22 @@ LABEL_BEGIN:
 	mov	byte[LABEL_DESC_CODE_RING3 + 4],al
 	mov	byte[LABEL_DESC_CODE_RING3 + 7],ah
 
+	;初始化TSS描述符
+	xor	eax,eax
+	mov	ax,ds
+	shl	eax,4
+	add	eax,LABEL_TSS
+	mov	word[LABEL_DESC_TSS + 2],ax
+	shr	eax,16
+	mov	byte[LABEL_DESC_TSS + 4],al
+	mov	byte[LABEL_DESC_TSS + 7],ah
+
 	;为加载GDTR作准备
 	xor	eax,eax
 	mov	ax,ds
 	shl	eax,4
 	add	eax,LABEL_GDT				;eax<-GDT基地址
-	mov	dword [GdtPtr + 2],eax			;[GdtPtr + 2]<-GDT基地址
+	mov	dword[GdtPtr + 2],eax			;[GdtPtr + 2]<-GDT基地址
 
 	;加载GDTR
 	lgdt	[GdtPtr]
@@ -254,11 +301,16 @@ LABEL_SEG_CODE32:
 	
 	call	DispReturn
 	
+	mov	ax,SelectorTSS
+	ltr	ax			;装载任务状态段寄存器TR
+
 	push	SelectorStack3
 	push	TopOfStack3
 	push	SelectorCodeRing3
 	push	0
 	retf
+
+	ud2	;
 
 	;调用测试门（无特权级变换），将打印字母'C'
 	call	SelectorCallGateTest:0
@@ -300,8 +352,11 @@ LABEL_SEG_CODE_DEST:
 	mov	ah,0Ch
 	mov	al,'C'
 	mov	[gs:edi],ax
-
-	retf
+	
+	;Load LDT
+	mov	ax,SelectorLDT
+	lldt	ax
+	jmp	SelectorLDTCodeA:0	;跳入局部任务，打印字母'L'
 
 SegCodeDestLen	equ	$ - LABEL_SEG_CODE_DEST
 ;END of [SECTION .sdest]
@@ -373,6 +428,8 @@ LABEL_CODE_RING3:
 	mov	ah,0Ch
 	mov	al,'3'
 	mov	[gs:edi],ax
+	
+	call	SelectorCallGateTest:0
 
 	jmp	$
 SegCodeRing3Len	equ	$ - LABEL_CODE_RING3
